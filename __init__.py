9e24b6587f978de7efa13081f02c8932273d5332
nathanael <nattzwc@gmail.com>
2022-06-10 20:50:50 +0800

    dashboard stuff & error

--- __init__.py
+++ __init__.py
@@ -7,8 +7,30 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
-    print("plswork")
+    return render_template('500.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+# Internal Server Error
+@app.errorhandler(500)
+def error500(e):
+    return render_template('500.html'), 500
+
+
+# Internal Server Error
+@app.errorhandler(403)
+def error403(e):
+    return render_template('403.html'), 403
 
 
 if __name__ == '__main__':
