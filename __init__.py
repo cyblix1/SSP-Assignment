ba1c5cbef66ace96c181aef8e640138f7943f7b5
nathanael <nattzwc@gmail.com>
2022-06-10 14:02:21 +0800

    .

--- __init__.py
+++ __init__.py
@@ -4,5 +4,11 @@ import MySQLdb.cursors
 
 app = Flask(__name__)
 
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
