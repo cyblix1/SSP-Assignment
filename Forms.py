be510c0ac76a65ebe9dc3060345db738867b4e2b
nathanael <nattzwc@gmail.com>
2022-06-22 17:21:40 +0800

    .

--- Forms.py
+++ Forms.py
@@ -1,5 +1,6 @@
+import email
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError, TextAreaField
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError, TextAreaField, EmailField
 from wtforms.validators import DataRequired, EqualTo, Length
 from wtforms.widgets import TextArea
 from flask_ckeditor import CKEditorField
@@ -10,3 +11,11 @@ from flask_wtf.file import FileField
 # 	username = StringField("Username", validators=[DataRequired()])
 # 	password = PasswordField("Password", validators=[DataRequired()])
 # 	submit = SubmitField("Submit")
+
+class MakeAdminForm(FlaskForm):
+    name = StringField("Name", validators=[Length(min=1, max=50),DataRequired()])
+    email = EmailField("Email", validators=[Length(min=5, max=100),DataRequired()])
+    phone = StringField("Phone No", validators=[Length(min=8, max=8, message='Please enter a real number'),DataRequired()])
+    password1 = PasswordField("Password:", validators=[Length(min=8), DataRequired()])
+    password2 = PasswordField("Confirm Password", validators=[EqualTo('password1'), DataRequired()])
+    submit = SubmitField("Submit")
--- __init__.py
+++ __init__.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session,flash
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
-
+from flask_bcrypt import Bcrypt
+from datetime import datetime
+from Forms import *
 app = Flask(__name__)
+
+app.secret_key = "SSP"
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+#change to ur own password
+app.config['MYSQL_PASSWORD'] = '5zbhihcxqxeS'
+app.config['MYSQL_DB'] = 'sspassignment'
+db = MySQL(app)
+bcrypt = Bcrypt()
+
 
 @app.route('/')
 @app.route('/home')
@@ -12,7 +24,41 @@ def home():
 # @loginrequired here
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('base_admin.html')
+
+@app.route('/admins')
+def admins():
+    cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM admin_accounts')
+    all_data = cursor.fetchall()
+    return render_template('admins.html', employees = all_data)
+
+@app.route('/create_admin', methods=['POST'])
+def create_admin():
+    form = MakeAdminForm(request.form)
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        password = request.form['password']
+        hashedpw = bcrypt.generate_password_hash(password)
+        date_created = datetime.utcnow()
+        #checks if email is aready exists
+        cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+        if cursor.execute('SELECT * FROM admin_accounts WHERE email = %s', (email)):
+            flash('Email Exists')
+            return redirect(url_for('admins'))
+        elif cursor.execute('SELECT * FROM admin_accounts WHERE phone = %s', (phone)):
+            flash('Phone Number exists')
+            return redirect(url_for('admins'))
+        else:
+            #make account
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s, %s)', (name,email,phone,hashedpw,date_created))
+            db.connection.commit()
+            flash("Employee Inserted Successfully")
+            return redirect(url_for('admins'))
+
+
 
 
 # Invalid URL
