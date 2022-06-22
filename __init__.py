34f6409c8d4c7aeb4a90f41b80baf7e4ccc3e3af
nathanael <nattzwc@gmail.com>
2022-06-22 19:14:50 +0800

    .

--- __init__.py
+++ __init__.py
@@ -21,21 +21,19 @@ bcrypt = Bcrypt()
 def home():
     pass
 
-# @loginrequired here
+#base template
 @app.route('/dashboard')
 def dashboard():
     return render_template('base_admin.html')
 
-@app.route('/admins')
+@app.route('/admins' ,methods=['POST','GET'])
 def admins():
+    form = CreateAdminForm(request.form)
     cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute('SELECT * FROM admin_accounts')
     all_data = cursor.fetchall()
-    return render_template('admins.html', employees = all_data)
 
-@app.route('/create_admin', methods=['POST'])
-def create_admin():
-    form = MakeAdminForm(request.form)
+    #create employee
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
@@ -43,20 +41,56 @@ def create_admin():
         password = request.form['password']
         hashedpw = bcrypt.generate_password_hash(password)
         date_created = datetime.utcnow()
-        #checks if email is aready exists
+        #simple first later check is exists
         cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
-        if cursor.execute('SELECT * FROM admin_accounts WHERE email = %s', (email)):
-            flash('Email Exists')
-            return redirect(url_for('admins'))
-        elif cursor.execute('SELECT * FROM admin_accounts WHERE phone = %s', (phone)):
-            flash('Phone Number exists')
-            return redirect(url_for('admins'))
-        else:
-            #make account
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s, %s)', (name,email,phone,hashedpw,date_created))
-            db.connection.commit()
-            flash("Employee Inserted Successfully")
-            return redirect(url_for('admins'))
+        #make account
+        cursor.execute('INSERT INTO admin_accounts VALUES (NULL, %s, %s, %s, %s, %s)', (name,email,phone,hashedpw,date_created))
+        db.connection.commit()
+        flash("Employee Inserted Successfully")
+        return redirect(url_for('admins'))
+
+    return render_template('admins.html', employees = all_data,form=form)
+
+# @app.route('/create_admin', methods=['POST'])
+# def create_admin():
+#     form = CreateAdminForm(request.form)
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         email = request.form['email']
+#         phone = request.form['phone']
+#         password = request.form['password']
+#         hashedpw = bcrypt.generate_password_hash(password)
+#         date_created = datetime.utcnow()
+#         #simple first later check is exists
+#         cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+#         #make account
+#         cursor.execute('INSERT INTO admin_accounts VALUES (NULL, %s, %s, %s, %s, %s)', (name,email,phone,hashedpw,date_created))
+#         db.connection.commit()
+#         flash("Employee Inserted Successfully")
+#         return redirect(url_for('admins'),form=form)
+    
+
+
+@app.route('/update_admin', methods=['GET','POST'])
+def update():
+    if request.method == 'POST':
+        id = request.form.get('id')
+        cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        cursor.execute('UPDATE admin_accounts SET name = %s, email = %s, phone=%s WHERE id = %s', (name,email,phone,id))
+        db.connection.commit()
+        flash("Employee updated successfully")
+        return redirect(url_for('admins'))
+
+@app.route('/delete_admin/<id>/',  methods=['GET','POST'])
+def delete(id):
+    cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('DELETE FROM admin_accounts WHERE id = %s', (id))
+    db.connection.commit()
+    flash("Employee deleted successfully")
+    return redirect(url_for('admins'))
 
 
 
