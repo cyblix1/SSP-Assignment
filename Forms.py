11a755a783387870a5e2128a8d846a1d865f4ae0
nathanael <nattzwc@gmail.com>
2022-06-27 10:56:33 +0800

    .

--- Forms.py
+++ Forms.py
@@ -1,6 +1,6 @@
 import email
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError, TextAreaField, EmailField
+from wtforms import StringField, SubmitField, IntegerField, PasswordField, BooleanField, ValidationError, TextAreaField, EmailField
 from wtforms.validators import DataRequired, EqualTo, Length
 from wtforms.widgets import TextArea
 from flask_ckeditor import CKEditorField
@@ -12,12 +12,13 @@ class CreateAdminForm(FlaskForm):
     email = EmailField("Email", validators=[Length(min=5, max=100),DataRequired()])
     phone = StringField("Phone No", validators=[Length(min=8, max=8, message='Please enter a real number'),DataRequired()])
     description = TextAreaField("description",validators=[Length(max=200)])
-    password1 = PasswordField("Password:", validators=[Length(min=8), DataRequired(),EqualTo('password2')])
+    password1 = PasswordField("Password:", validators=[DataRequired(), EqualTo('password2')])
     password2 = PasswordField("Confirm Password", validators=[DataRequired()])
     submit = SubmitField("Add Employee")
 
 
 class UpdateAdminForm(FlaskForm):
+    id = IntegerField("Id")
     name = StringField("Name", validators=[Length(min=1, max=50),DataRequired()])
     email = EmailField("Email", validators=[Length(min=5, max=100),DataRequired()])
     phone = StringField("Phone No", validators=[Length(min=8, max=8, message='Please enter a real number'),DataRequired()])
--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session,fl
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 from flask_bcrypt import Bcrypt
-from datetime import datetime
+from datetime import datetime, timedelta
 from Forms import *
 from configparser import ConfigParser
 
@@ -18,6 +18,7 @@ app.config['MYSQL_HOST'] = config['account']['host']
 app.config['MYSQL_USER'] = config['account']['user']
 app.config['MYSQL_PASSWORD'] = config['account']['password']
 app.config['MYSQL_DB'] = config['account']['db']
+app.permanent_session_lifetime = timedelta(minutes=10)
 db = MySQL(app)
 bcrypt = Bcrypt()
 
@@ -36,50 +37,55 @@ def home():
 def dashboard():
     return render_template('base_admin.html')
 
-@app.route('/admins')
+@app.route('/admins', methods=['POST','GET'])
 def admins():
     form = CreateAdminForm()
     form2 = UpdateAdminForm()
     cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute('SELECT * FROM admin_accounts')
     all_data = cursor.fetchall()
+    if request.form == 'POST'and form.validate_on_submit():
+        return redirect(url_for('create_admin'))
+    if request.form == 'POST' and form2.validate_on_submit():
+        return redirect(url_for('update_admin'))
     return render_template('admins.html', employees = all_data, form = form, form2=form2)
 
-@app.route('/admins/create_admin', methods=['POST','GET'])
+@app.route('/admins/create_admin', methods=['POST'])
 def create_admin():
-    form = CreateAdminForm(request.form)
-    if request.method == 'POST':
-        form.validate_on_submit()
-        name = form.name.data
-        email = form.email.data
-        phone = form.phone.data
-        description = form.description.data
-        password = form.password1.data
-        hashedpw = bcrypt.generate_password_hash(password)
-        date_created = datetime.utcnow()
-        #simple first later check is exists
-        cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
-        #make account
-        cursor.execute('INSERT INTO admin_accounts VALUES (NULL, %s, %s, %s, %s, %s, %s)', (name,email,phone,hashedpw,date_created,description))
-        db.connection.commit()
-        flash("Employee Added Successfully!")
-        return redirect(url_for('admins'))
+    form = CreateAdminForm()
+    name = form.name.data
+    email = form.email.data
+    phone = form.phone.data
+    description = form.description.data
+    password = form.password1.data
+    password2 = form.password2.data
+    if password != password2:
+        flash('passwords does not match')
+        return redirect('admins')
+    hashedpw = bcrypt.generate_password_hash(password)
+    date_created = datetime.utcnow()
+    #simple first later check is exists
+    cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+    #make account
+    cursor.execute('INSERT INTO admin_accounts VALUES (NULL, %s, %s, %s, %s, %s, %s)', (name,email,phone,hashedpw,date_created,description))
+    db.connection.commit()
+    flash("Employee Added Successfully!")
+    return redirect(url_for('admins'))
 
 
 @app.route('/admins/update_admin', methods=['POST'])
 def update_admin():
     form = UpdateAdminForm()
-    if request.method == 'POST':
-        id = request.form.get('id')
-        cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
-        name = form.name.data
-        email = form.email.data
-        phone = form.phone.data
-        description = form.description.data
-        cursor.execute('UPDATE admin_accounts SET name = %s, email = %s, phone=%s, description=%s WHERE id = %s', (name,email,phone,description,id))
-        db.connection.commit()
-        flash("Employee updated successfully")
-        return redirect(url_for('admins'))
+    id = form.id.data
+    cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+    name = form.name.data
+    email = form.email.data
+    phone = form.phone.data
+    description = form.description.data
+    cursor.execute('UPDATE admin_accounts SET name = %s, email = %s, phone=%s, description=%s WHERE id = %s', (name,email,phone,description,id))
+    db.connection.commit()
+    flash("Employee updated successfully")
+    return redirect(url_for('admins'))
 
 @app.route('/admins/delete_admin/<int:id>/',  methods=['GET','POST'])
 def delete_admin(id):
@@ -89,6 +95,10 @@ def delete_admin(id):
     flash("Employee deleted successfully")
     return redirect(url_for('admins'))
 
+
+@app.route('/staff')
+def staff():
+    return render_template('staff.html')
 
 
 
