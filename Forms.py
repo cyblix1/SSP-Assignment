fb938a8eb3a6083a517ef9c9ec91985e61ad5868
samuelcham-cham <211836L@mymail.nyp.edu.sg>
2022-07-04 13:49:20 +0800

    register init

--- Forms.py
+++ Forms.py
@@ -28,10 +28,10 @@ class UpdateAdminForm(FlaskForm):
 # class AdminChangePassword(FlaskForm):
 
 class Register_Users (FlaskForm):
-    username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
-    # email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
-    password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
-    password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
-    submit = SubmitField(label='Create Account')
+    name = StringField("Name", validators=[Length(min=1, max=50), DataRequired()])
+    email = EmailField("Email", validators=[Length(min=5, max=100), DataRequired()])
+    password1 = PasswordField("Password:", validators=[DataRequired(), EqualTo('password2')])
+    password2 = PasswordField("Confirm Password", validators=[DataRequired()])
+    submit = SubmitField("Add Customer")
 
 
--- __init__.py
+++ __init__.py
@@ -36,25 +36,26 @@ def handle_csrf_error(e):
 @app.route('/register',methods =['GET', 'POST'])
 def register():
     form = Register_Users()
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+    if request.form == 'POST' and form.validate_on_submit():
+    # if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
         username = request.form['username']
         password = request.form['password']
         email = request.form['email']
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username,))
-        account = cursor.fetchone()
-        if account:
-            msg = 'Account already exists !'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address !'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers !'
-        elif not username or not password or not email:
-            msg = 'Please fill out the form !'
-        else:
-            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email,))
-            mysql.connection.commit()
-            msg = 'You have successfully registered !'
+        time = datetime.utcnow()
+        cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM customer_accounts WHERE username = % s', (username))
+        # if account:
+        #     msg = 'Account already exists !'
+        # elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        #     msg = 'Invalid email address !'
+        # elif not re.match(r'[A-Za-z0-9]+', username):
+        #     msg = 'Username must contain only characters and numbers !'
+        # elif not username or not password or not email:
+        #     msg = 'Please fill out the form !'
+        # else:
+
+        cursor.execute('INSERT INTO customer_accounts VALUES (NULL, %s, %s, %s, %s, %s)', (username,email, password,time))
+        db.connection.commit()
     elif request.method == 'POST':
         msg = 'Please fill out the form !'
     return render_template('register.html',form=form)
