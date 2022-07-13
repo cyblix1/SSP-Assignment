1b89ee11ccf55b6f7e6314912300c62ff2cac645
nathanael <nattzwc@gmail.com>
2022-07-13 10:57:01 +0800

    q

--- Forms.py
+++ Forms.py
@@ -96,3 +96,29 @@ class Register_Users(FlaskForm):
     password1 = PasswordField("Password:", validators=[DataRequired(), EqualTo('password2')])
     password2 = PasswordField("Confirm Password")
     submit = SubmitField("Add Customer")
+
+class Create_Products(FlaskForm):
+    product_name = StringField(label='Name', validators=[Length(min=1, max=100), DataRequired()])
+    description = TextAreaField(label='Description', validators=[DataRequired(), Length(min=1, max=1000)])
+    price = StringField(label='Price', validators=[DataRequired(), Length(min=1)])
+    submit = SubmitField(label='Add Item')
+
+class Update_Products(FlaskForm):
+    product_id = IntegerField("Id",validators=[DataRequired()])
+    product_name = StringField(label='Name', validators=[Length(min=1, max=100), DataRequired()])
+    description = TextAreaField(label='Description', validators=[DataRequired(), Length(min=1, max=1000)])
+    price = StringField(label='Price', validators=[DataRequired(), Length(min=1)])
+    submit = SubmitField(label = "Save Changes")
+
+class LoginForm(FlaskForm):
+    customer_name = StringField("Name", validators=[Length(min=1, max=50), DataRequired()])
+    password1 = PasswordField("Password:", validators=[DataRequired()])
+    submit = SubmitField(label = "Add Customer")
+
+class Add_To_Cart (FlaskForm):
+    product_id = IntegerField("Id", validators=[DataRequired()])
+    product_name = StringField(label='Name', validators=[Length(min=1, max=100), DataRequired()])
+    description = TextAreaField(label='Description', validators=[DataRequired(), Length(min=1, max=1000)])
+    price = StringField(label='Price', validators=[DataRequired(), Length(min=1)])
+    submit = SubmitField(label="Save Changes")
+    submit = SubmitField("Add Customer")
