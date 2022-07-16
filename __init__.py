a9b08f8795b53e4341ba7dd18e6bd6cf8b4e2061
nathanael <nattzwc@gmail.com>
2022-07-16 18:36:12 +0800

    .

--- __init__.py
+++ __init__.py
@@ -19,9 +19,8 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from validations import Validations
-
-
+from validations import *
+from verify import *
 
 
 app = Flask(__name__)
